71f35cd21b013e40ce05665c98ef547af9742f71
Rushali Jaiswal <rushali30@gmail.com>
2017-07-31 01:05:51 -0400

    update/delete database

--- src/csps/src/coachsafeplaysafe.py
+++ src/csps/src/coachsafeplaysafe.py
@@ -12,7 +12,7 @@ import sqlite3
 import os
 
 
-no_sensor_api = False
+mnnno_sensor_api = False
 no_sensor = False
 notEstablished = True
 
@@ -23,8 +23,8 @@ if os.name == 'posix':
 class StudentListButton(ListItemButton):
     pass
 
-
 class ScreenOne(Screen):
+    title = 'Zero'
    
     fn = ObjectProperty()
     ln = ObjectProperty()
@@ -40,7 +40,7 @@ class ScreenOne(Screen):
         
         con = sqlite3.connect('players.db')
         cur = con.cursor() 
-        cur.execute("Create TABLE if not exists Players(first_name TEXT primary key, last_name TEXT unique)")
+        cur.execute("Create TABLE if not exists Players(first_name TEXT, last_name TEXT)")
         cur.execute("INSERT INTO Players (first_name, last_name) VALUES (?,?)", (first_name, last_name))
         con.commit()   
         cur.execute("SELECT * FROM Players")
@@ -51,97 +51,37 @@ class ScreenOne(Screen):
             selection = self.student_list.adapter.selection[0].text
             self.student_list.adapter.data.remove(selection)
             self.student_list._trigger_reset_populate()
-        
+
         first_name = self.fn.text
         last_name =self.ln.text
+        con = sqlite3.connect('players.db')
+        cur = con.cursor() 
+        cur.execute("SELECT * FROM Players")
+        cur.execute('DELETE FROM Players WHERE first_name=(?) and last_name=(?)', (first_name, last_name))
+        con.commit()
+
 
     def replace_student(self, *args):
         if self.student_list.adapter.selection:
+
             selection = self.student_list.adapter.selection[0].text
             self.student_list.adapter.data.remove(selection)
             student_name = self.fn.text +" "+ self.ln.text
             self.student_list.adapter.data.extend([student_name])
             self.student_list._trigger_reset_populate()
+            print (selection)
+
+        first_name = self.fn.text
+        last_name =self.ln.text
+        con = sqlite3.connect('players.db')
+        cur = con.cursor() 
+        cur.execute("SELECT * FROM Players")
+        cur.execute('UPDATE Players SET first_name=(?) WHERE first_name=(?)', (first_name))
+        con.commit()
 
 
 class ScreenTwo(Screen):
-    gyroX = ListProperty([])
-    gyroY = ListProperty([])
-    gyroZ = ListProperty([])
-
-    accelX = ListProperty([])
-    accelY = ListProperty([])
-    accelZ = ListProperty([])
-
-    values = ListProperty([])
-
-    def __init__(self, **kwargs):
-        super(ScreenTwo, self).__init__(**kwargs)
-        global no_sensor, no_sensor_api
-        self.tssensor = None
-        if not no_sensor_api:
-            self.com_port = None
-            self.device_list = ts_api.getComPorts()
-            if len(self.device_list) > 0:
-                for self.device in self.device_list:
-                    self.cp = self.device.com_port
-                    self.port_info = ts_api.getDeviceInfoFromComPort(self.cp, poll_device=True)
-                    if self.port_info.dev_type == 'BT-H3' or self.port_info.dev_type == 'BT':
-                        self.com_port = self.cp
-                        self.tssensor = ts_api.TSBTSensor(self.com_port)
-                        break
-                    if self.port_info.dev_type == 'DNG':
-                        self.com_port = self.cp
-                        self.tsdongle = ts_api.TSDongle(self.com_port)
-                        if not self.tsdongle:
-                            no_sensor = True
-                            break
-                        self.tssensor = self.tsdongle.getSensorFromDongle(0)
-                        if not self.tssensor:
-                            no_sensor = True
-                            break
-                        break
-                    if self.port_info.dev_type == 'WL-H3' or self.port_info.dev_type == 'WL':
-                        self.com_port = self.cp
-                        self.tssensor = ts_api.TSWLSensor(self.com_port)
-                        break
-                if not self.com_port:
-                    no_sensor = True
-            else:
-                no_sensor = True
-        else:
-            no_sensor = True
-            no_sensor_api = True
-            print ("No Sensor API")
-
-    def add_running_values(self, dt):
-        global notEstablished, no_sensor
-
-        if not no_sensor:
-            print ("zxc")
-            data = self.tssensor.getCorrectedGyroRate()
-            self.gyroX.append(data[0])
-            self.gyroX = self.gyroX[-100:]
-            self.gyroY.append(data[1])
-            self.gyroY = self.gyroY[-100:]
-            self.gyroZ.append(data[2])
-            self.gyroZ = self.gyroZ[-100:]
-
-            data = self.tssensor.getCorrectedAccelerometerVector()
-            self.accelX.append(data[0])
-            self.accelX = self.accelX[-100:]
-            self.accelY.append(data[1])
-            self.accelY = self.accelY[-100:]
-            self.accelZ.append(data[2])
-            self.accelZ = self.accelZ[-100:]
-        else:
-            self.values.append(gauss(.5, .1))
-            self.values = self.values[-100:]
-            if notEstablished:
-                print ("Sensor not Connected")
-                notEstablished = False
-
-
+    pass
 class CoachSafePlaySafeApp(App):
     def build(self):
         screen_manager = ScreenManager(transition=SwapTransition())
